dff910b3bb7a1f650c3fa12b9f5b9ac1f479a938
sharmilamaheshbalurakshitha <144773893+sharmilamaheshbalurakshitha@users.noreply.github.com>
2023-09-12 12:35:43 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,12 @@
+Year=int(input("Enter year to bechecked: "))
+
+if year%4==0:
+ if year%100==0:
+   if year%400==0:
+         Print("The year is a leap year!")
+else:
+   Print("The year is not a leap year!")
+else:
+   Print("The year is a leap year!")
+else:
+    Print("The year is not a leap year!")
